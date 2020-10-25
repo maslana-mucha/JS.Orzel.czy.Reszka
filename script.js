f2171d9727d4276c4602f49846144ca2c70c345e
maslana.m <maslana.m89@gmail.com>
2020-10-26 00:43:37 +0100

    Adding results panel

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
 const btn = document.querySelector('button');
 const divResult = document.querySelector('.result');
 const divCounter = document.querySelector('.counter');
+const divFinalResults = document.querySelector('.final-results');
 
 const choices = ['orzeł', 'reszka'];
+const resultsArr = [];
 let count = 0;
 
+const results = () => {
+  resultsArr.length = 3;
+  console.log(resultsArr);
+  const newArr = resultsArr.filter((e) => e > 0);
+  console.log(newArr);
+  if (newArr.length > 1) {
+    divFinalResults.textContent = 'Wygrała reszka';
+  } else {
+    divFinalResults.textContent = 'Zwycięski orzeł';
+  }
+};
+
 const counter = () => {
+  divFinalResults.textContent = '';
   count++;
   divCounter.textContent = 'Rzut nr: ' + count;
   if (count === 4) {
     count = 0;
     divCounter.textContent = 'Do trzech razy sztuka :P';
     divResult.textContent = '';
+    results();
+    resultsArr.length = 0;
   }
 };
 
 const draw = () => {
   const result = Math.floor(Math.random() * choices.length);
-
+  resultsArr.push(result);
   divResult.textContent = choices[result];
   counter();
 };
